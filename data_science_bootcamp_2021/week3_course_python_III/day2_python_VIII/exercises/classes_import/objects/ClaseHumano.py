e93756929f6d10cafd38936a92b7d70498cce269
maryc-11 <maryc-11@hotmail.com>
2021-04-29 20:53:45 +0200

    Day 4 and task

--- data_science_bootcamp_2021/week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseHumano.py
+++ data_science_bootcamp_2021/week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseHumano.py
@@ -1,6 +1,6 @@
-class Humano:
+class Humano ():
 
-    def __init__(self,  nombre, armadura, nivel, ataque, ojos=2, piernas=2, dientes=32, salud=100)
+    def __init__(self,  nombre, armadura, nivel, ataque, ojos=2, piernas=2, dientes=32, salud=100):
         self.nombre = nombre
         self.armadura = armadura
         self.nivel = nivel
@@ -13,16 +13,17 @@ class Humano:
 #Esta función resta a la vida del 'Orco' el ataque de 'Humano' 
 # menos la armadura de 'Orco'. Al final, muestra por pantalla la vida del 
 # 'Orco'
-    def atacar(orco):
-        Orco.salud -= self.ataque - Orco.armadura
-        print("La salud del Orco es", Orco.salud)
+    def atacar(self,orco):
+        
+        orco.salud -= self.ataque - orco.armadura
+        print("La salud del Orco es", orco.salud)
 
 #Una función no_vivo que retorna True si la vida del 'Humano' 
 # es igual o inferior a 0, False en otro caso. 
 
-    def no_vivo(self,salud):
-        self.salud = salud
-        if salud =< 0:
+    def no_vivo(self):
+        self.salud
+        if self.salud <= 0:
             return True
         else:
             return False
@@ -35,11 +36,9 @@ class Humano:
 
     def mostrar_atributos(self):
         print("nombre:", self.nombre, "/", "dientes:", self.dientes, "/", "ojos:", self.ojos, 
-        "/", "piernas:", self.piernas, "/", "armadura:", self.armadura, "/", "nivel:", self.nivel, "/", "ataque:",
-        self.ataque, "/", "salud",self.salud  )
+        "/", "piernas:", self.piernas, "/", "armadura:", self.armadura, "/", "nivel:", self.nivel, "/", "ataque:", self.ataque, "/", "salud",self.salud)
   
        
-        
 
 
 
--- data_science_bootcamp_2021/week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseOrco.py
+++ data_science_bootcamp_2021/week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseOrco.py
@@ -1,6 +1,6 @@
-class Orco:
+class Orco():
 
-    def __init__(self,  nombre, armadura, nivel, ataque, ojos=2, piernas=2, dientes=56, salud=100)
+    def __init__(self,  nombre, armadura, nivel, ataque, ojos=2, piernas=2, dientes=56, salud=100):
         self.nombre = nombre
         self.armadura = armadura
         self.nivel = nivel
@@ -11,14 +11,13 @@ class Orco:
         self.salud = salud
 
     
-    def atacar(Humano):
-        Humano.salud -= self.ataque - Orco.armadura
-        print("La salud del Orco es", Orco.salud)
+    def atacar(self,humano):
+        humano.salud -= self.ataque - humano.armadura
+        print("La salud del Humano es", humano.salud)
 
     
-    def no_vivo(self,salud):
-        self.salud = salud
-        if salud =< 0:
+    def no_vivo(self):
+        if self.salud <= 0:
             return True
         else:
             return False
@@ -26,4 +25,5 @@ class Orco:
     def mostrar_atributos(self):
         print("nombre:", self.nombre, "/", "dientes:", self.dientes, "/", "ojos:", self.ojos, 
         "/", "piernas:", self.piernas, "/", "armadura:", self.armadura, "/", "nivel:", self.nivel, "/", "ataque:",
-        self.ataque, "/", "salud",self.salud  )
+        self.ataque, "/", "salud",self.salud)
+
